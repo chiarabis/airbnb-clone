de8df606ecc406ad617dc84cf0ba56fee12725c1
Chiara <124071052+chiarabis@users.noreply.github.com>
2023-11-06 11:02:33 +0100

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,4 @@
 import React from 'react'
-//import yogaLessonImage from '../images/yoga-lesson.jpg'
-//import hikingTripImage from '../images/hiking-trip.jpg'
-//import divingImage from '../images/diving.jpg'
-//import wineTastingImage from '../images/wine-tasting.jpg'
 import starRatingIcon from '../images/star-rating.png'
 
 export default function Card(props){
@@ -28,37 +24,3 @@ export default function Card(props){
         </div>
     )
 }
-
-/*<div className='card'>
-    <img src={hikingTripImage} alt='hiking' className='card-image'></img>
-    <div className='card-stats'>
-        <img src={starRatingIcon} alt='star' className='card-star'></img>
-        <span>4.8</span>
-        <span className='gray'>(10) •</span>
-        <span className='gray'>Chile</span>
-    </div>
-    <p>Two hours mountain hike</p>
-    <p><span className='bold'>From 20$</span> / person</p>
-</div>
-<div className='card'>
-    <img src={divingImage} alt='diving' className='card-image'></img>
-    <div className='card-stats'>
-        <img src={starRatingIcon} alt='star' className='card-star'></img>
-        <span>4.9</span>
-        <span className='gray'>(13) •</span>
-        <span className='gray'>Australia</span>
-    </div>
-    <p>Discover the Great Barrier Reef</p>
-    <p><span className='bold'>From 105$</span> / person</p>
-</div>
-<div className='card'>
-    <img src={wineTastingImage} alt='wine-tasting' className='card-image'></img>
-    <div className='card-stats'>
-        <img src={starRatingIcon} alt='star' className='card-star'></img>
-        <span>4.6</span>
-        <span className='gray'>(9) •</span>
-        <span className='gray'>Italy</span>
-    </div>
-    <p>A real italian experience</p>
-    <p><span className='bold'>From 34$</span> / person</p>
-</div>*/
